f06d590a06bdebda7173e60567475fb0fc9feb1d
unknown <xxx_darkforces@mail.ru>
2022-11-09 12:30:04 +0700

    edit

--- task2/script.js
+++ task2/script.js
@@ -3,3 +3,17 @@ const consoleLog = document.querySelector('#consoleLog');
 consoleLog.addEventListener('click', () => {
     alert('Служит для вывода информации в консоль');
 })
+
+const alertid = document.querySelector('#alert');
+
+alertid.addEventListener('click', () => {
+    alert('Показывает диалоговое окно с сообщением и кнопкой OK');
+})
+
+const promptid = document.querySelector('#prompt');
+
+promptid.addEventListener('click', () => {
+    const promptid = prompt('Отображает диалоговое окно с запросом на ввод текста');
+    console.log(promptid);
+})
+
