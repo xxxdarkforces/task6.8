893a01a8a1677ec09a27a728c9816c8e2051d288
unknown <xxx_darkforces@mail.ru>
2022-11-10 10:49:02 +0700

    edit

--- task5/script.js
+++ task5/script.js
@@ -1,11 +1,19 @@
 const buttonEvent = document.querySelector('button');
 const inputForm = document.querySelector('form');
 const inputText = document.querySelector('input');
+const hrshelf = document.querySelector('hr');
 
 
 buttonEvent.style.background = '#a8e4a0';
+buttonEvent.style.borderRadius = '5px';
 
-inputForm.style.borderRadius = '5px';
+inputText.style.borderRadius = '5px';
+
+hrshelf.style.margin = '20px 0';
+hrshelf.style.padding = '0';
+hrshelf.style.height = '5px';
+hrshelf.style.border = 'none';
+hrshelf.style.background = 'linear-gradient(180deg, #a8e4a0, blue)';
 
 inputForm.addEventListener('submit', (event) => {
     event.preventDefault();
